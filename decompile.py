54e6e60481173e6151ff5b999136dfbcd587acaa
HAvlani <99096999+HAvlani@users.noreply.github.com>
2022-06-05 17:59:37 +0530

    Update decompile.py

--- decompile.py
+++ decompile.py
@@ -13,15 +13,15 @@ from ghidra.util.task import ConsoleTaskMonitor
 
 program = getCurrentProgram()
 ifc = DecompInterface()
-ifc.openProgram(Program)
-func = getFirstFunction()
+ifc.openProgram(program)
+function = getFirstFunction()
 i=1
 
 
-while func is not None:
+while function is not None:
     results = ifc.decompileFunction(function, 0, ConsoleTaskMonitor())
-    output += results.getDecompiledFunction().getC()
-    name = str("function " + i)
+    output = results.getDecompiledFunction().getC()
+    name = "function " + str(i)
     i+=1
     function = getFunctionAfter(function)
     with open(name, "w") as f:
