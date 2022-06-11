450461ec49a9d69e9f9a943b1cd4632e409d0f79
HAvlani <99096999+HAvlani@users.noreply.github.com>
2022-06-11 21:05:43 +0530

    Update angrtest.py

--- angr/angrtest.py
+++ angr/angrtest.py
@@ -8,10 +8,14 @@ i=1
 p = angr.Project('/Users/harshilavlani/Headless_decomp/2function', auto_load_libs=False, load_debug_info=True)
 cfg = p.analyses[CFGFast].prep()(data_references=True, normalize=True)
 for f in cfg.functions.values():
+    
     dec = p.analyses[Decompiler].prep()(f, cfg=cfg.model)
+    print(dec)
     name = "function" + str(i)
     i+=1
     filepath = os.path.join('/Users/harshilavlani/Headless_decomp/decomp_funcs_angr/'+ name)
-    #with open(filepath, "w") as f:
-        #f.write(dec.codegen.text)
-    print(dec.codegen)
+    try:
+        with open(filepath, "w") as f:
+            f.write(dec.codegen.text)
+    except:
+        continue
