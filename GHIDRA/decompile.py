1bc133fac852033c49a0ae3cb8172c9883bb7a2a
HAvlani <99096999+HAvlani@users.noreply.github.com>
2022-06-22 21:29:19 -0700

    Update decompile.py

--- GHIDRA/decompile.py
+++ GHIDRA/decompile.py
@@ -1,11 +1,3 @@
-#
-#@author  Harshil
-#@category Test
-#@keybinding
-#@menupath
-#@toolbar
-
-#TODO: Add script code here
 from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
 import os
@@ -22,9 +14,10 @@ i=1
 while function is not None:
     results = ifc.decompileFunction(function, 0, ConsoleTaskMonitor())
     output = results.getDecompiledFunction().getC()
-    name = "function " + str(i)
+    name = function.getName() + " " + program.getName()
     i+=1
     function = getFunctionAfter(function)
     filepath = os.path.join('/Users/harshilavlani/Headless_decomp/decomp_funcs_Ghidra/'+ name)
     with open(filepath, "w") as f:
         f.write(output)
+
