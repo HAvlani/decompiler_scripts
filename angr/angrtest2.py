37463624c242efe88aab821db63b1220b70b8fae
HAvlani <99096999+HAvlani@users.noreply.github.com>
2022-06-25 14:22:19 -0700

    Update angrtest2.py

--- angr/angrtest2.py
+++ angr/angrtest2.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ProcessPoolExecutor
+from socket import timeout
 import angr
 from angr.analyses import (
     CFGFast,
@@ -6,51 +6,44 @@ from angr.analyses import (
 )
 from claripy import *
 import os
+from joblib import Parallel, delayed
+import timeout_decorator
+
 #import ipdb
 #ipdb.set_trace()
-executor = ProcessPoolExecutor()
-#for filename in os.listdir('/Users/harshilavlani/Headless_decomp/coreutils-builder/bin/'):
-
-          
-            
-
-
 def decompile(filename):
     p = angr.Project('/Users/harshilavlani/Headless_decomp/coreutils-builder/bin/' + filename, auto_load_libs=False, load_debug_info=True)
     cfg = p.analyses[CFGFast].prep()(data_references=True, normalize=True)
     for f in cfg.functions.values():
-        
         print(f.name + " " + filename)
-        
-            
         name = filename + " " + f.name
         filepath = os.path.join('/Users/harshilavlani/Headless_decomp/decomp_funcs_angr/'+ name)
         try:    
             try:
+
                 dec = p.analyses[Decompiler].prep()(f, cfg=cfg.model)
-            
                 with open(filepath, "w") as f:
                     f.write(dec.codegen.text)
             except AttributeError: 
                 with open('/Users/harshilavlani/Headless_decomp/angr-errors/AttributeError', "a") as f:
                     f.write(name + "\n")
-                #print("*************" + "AttributeError " + name + "************")
+      
         except ValueError:
             with open('/Users/harshilavlani/Headless_decomp/angr-errors/ValueError', "a") as f:
                     f.write(name + "\n")
-            #print("*************" + "ValueError " + f.name + filename + "************")
         except ClaripyOperationError:
             with open('/Users/harshilavlani/Headless_decomp/angr-errors/ClaripyOperationError', "a") as f:
                     f.write(name + "\n")
-            #print("*************" + "ClaripyError " + f.name + filename + "************")
         except TypeError:
             with open('/Users/harshilavlani/Headless_decomp/angr-errors/TypeError', "a") as f:
                     f.write(name + "\n")
-            #print("*************" + "TypeError " + f.name + filename + "************")
-        
+
             
-    
         
+Parallel(n_jobs=4, timeout=5, verbose=10)(delayed(decompile)(filename) for filename in os.listdir('/Users/harshilavlani/Headless_decomp/coreutils-builder/bin/'))
+
+#with Parallel(n_jobs=4) as parallel:
     
-for filename in os.listdir('/Users/harshilavlani/Headless_decomp/coreutils-builder/bin/'):
-    executor.submit(decompile(filename))
+    
+    #if __name__ == '__main__':
+        #parallel(delayed(decompile)(filename) for filename in os.listdir('/Users/harshilavlani/Headless_decomp/coreutils-builder/bin/'))
