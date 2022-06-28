345a28e4656dbfcdfad90002a38ceecf2b500c8c
HAvlani <99096999+HAvlani@users.noreply.github.com>
2022-06-28 00:15:16 -0700

    Update decompile.py

--- GHIDRA/decompile.py
+++ GHIDRA/decompile.py
@@ -8,14 +8,11 @@ program = getCurrentProgram()
 ifc = DecompInterface()
 ifc.openProgram(program)
 function = getFirstFunction()
-i=1
-
 
 while function is not None:
     results = ifc.decompileFunction(function, 0, ConsoleTaskMonitor())
     output = results.getDecompiledFunction().getC()
     name = function.getName() + " " + program.getName()
-    i+=1
     function = getFunctionAfter(function)
     filepath = os.path.join('/Users/harshilavlani/Headless_decomp/decomp_funcs_Ghidra/'+ name)
     with open(filepath, "w") as f:
