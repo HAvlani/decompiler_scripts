eb68e27a6b261f322d69738688c3765b99f94f52
HAvlani <harshilavlani@gmail.com>
2022-07-13 07:27:20 -0700

    Add angr measurement

--- addmccall.py
+++ addmccall.py
@@ -2,28 +2,42 @@ import os
 import delegator
 from tqdm import tqdm
 import json
-TEXTPATH = '/Users/harshilavlani/Documents/Terminal Saved Output'
 GHIDRAFUNCTIONPATH = '/Users/harshilavlani/Headless_decomp/decomp_funcs_GHIDRA/'
+ANGRFUNCTIONPATH = '/Users/harshilavlani/Headless_decomp/decomp_funcs_angr/'
 totalmcc = 0
-finaldict= {}
+GHIDRAdict= {}
+angrdict = {}
 
 
 for filename in tqdm(os.listdir(GHIDRAFUNCTIONPATH)):
     GHIDRAFUNCTIONFILE = GHIDRAFUNCTIONPATH + filename
     c = delegator.run('pmccabe ' + GHIDRAFUNCTIONFILE)
-    try:
-        mcc = c.out.split()[1]
-        filename = c.out.split()[5].split('/')[-1].split('(')[0]
-        finaldict[filename] = mcc
-    except: 
-        with open('/Users/harshilavlani/Headless_decomp/angr-errors/' + 'MCCError', "a") as a:
-            a.write(filename + "\n")
+    #try:
+    mcc = c.out.split()[1]
+    filename = c.out.split()[5].split('/')[-1].split('(')[0]
+    GHIDRAdict[filename] = mcc
+    #except: 
+     #   with open('/Users/harshilavlani/Headless_decomp/Misc-errors/' + 'MCCError', "a") as a:
+      #      a.write(filename + "\n")
+        
+for filename in tqdm(os.listdir(ANGRFUNCTIONPATH)):
+    ANGRFUNCTIONFILE = ANGRFUNCTIONPATH + filename
+    c = delegator.run('pmccabe ' + ANGRFUNCTIONFILE)
+    #try:
+    mcc = c.out.split()[1]
+    filename = c.out.split()[5].split('/')[-1].split('(')[0]
+    angrdict[filename] = mcc
+    #except: 
+     #   with open('/Users/harshilavlani/Headless_decomp/Misc-errors/' + 'MCCError', "a") as a:
+      #      a.write(filename + "\n")
         
     
+
+with open("/Users/harshilavlani/Headless_decomp/GHIDRAmcc.json", "a") as outfile:
+    json.dump(GHIDRAdict, outfile)
     
-print(finaldict)
-with open("/Users/harshilavlani/Headless_decomp/mcc.json", "a") as outfile:
-    json.dump(finaldict, outfile)
+with open("/Users/harshilavlani/Headless_decomp/angrmcc.json", "a") as outfile:
+    json.dump(angrdict, outfile)
 #print(c.out)
 #with open(TEXTPATH, "r") as a:
 #    lines = a.read().splitlines()
